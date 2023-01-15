7a989f044aaad539f8c77bfd333007dcec353610
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-14 23:00:22 -0300

    Configurando rota de pedidos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavbarComponente/>
   <router-view/>
+  <FooterComponente/>
 </template>
+<script>
+import NavbarComponente from './components/NavbarComponente.vue';
+import FooterComponente from './components/FooterComponente.vue';
+export default {
+  name: "App",
+  components:{
+    NavbarComponente,
+    FooterComponente
+  }
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
