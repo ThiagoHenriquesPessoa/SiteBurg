0594555af8371dbb4acd9ae964b2170810644898
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-16 22:51:55 -0300

    Criacao da parte do banner

---
+++ src/components/BannerComponente.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="main-banner">
+     <h1>Make Your Burger</h1>
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'BannerConponente'
+  }
+  </script>
+
+  <style scoped>
+  #main-banner {
+    background-image: url('/public/Img/burger.jpg');
+    background-position: 0 -300px;
+    background-size: cover;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  #main-banner h1{
+    color: #fff;
+    text-align: center;
+    font-size: 60px;
+    padding: 20px;
+  }
+  </style>
