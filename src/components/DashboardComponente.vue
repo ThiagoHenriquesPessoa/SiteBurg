37d3575473f2be153db062bfd0c52a0e94ace1ab
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-01-25 23:19:30 -0300

    Preenchendo dinamicamente o dashboard

--- src/components/DashboardComponente.vue
+++ src/components/DashboardComponente.vue
@@ -11,15 +11,16 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>nome</div>
-                <div>Pão</div>
-                <div>carne:</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}:</div>
                 <div>
                     <ul>
-                        <li>opcional</li>
-                        <li>opcional</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{opcional}}
+                        </li>
                     </ul>
                 </div>
                 <div>
@@ -35,7 +36,28 @@
 
 <script>
 export default {
-    name: 'DashboardComponente'
+    name: 'DashboardComponente',
+    data(){
+        return{
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods:{
+        async getpedidos(){
+            const req = await fetch("http://localhost:3000/burgers");
+            const data = await req.json();
+            this.burgers = data;
+
+            console.log(this.burgers);
+
+            //resgatar os status
+        }
+    },
+    mounted(){
+        this.getpedidos();
+    }
 }
 </script>
 
